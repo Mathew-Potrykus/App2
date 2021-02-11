8a2ff17984ab40360354fffba6e096552f96a05b
Mathew Potrykus <mathew.potrykus@emcoruk.com>
2021-02-11 12:13:21 +0000

    first commit

---
+++ Reciever App/Reciever App/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Reciever_App
+{
+	public static class Helper
+	{
+		public static Action<string> Return { get; internal set; }
+
+		public static void SetReturn(Action<string> returnMethod)
+		{
+			if (Return == null)
+			{
+				Return = returnMethod;
+			}
+		}
+	}
+}
---
+++ Reciever App/Reciever App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Reciever_App.Models;
+using Reciever_App.ViewModels;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Reciever_App.Views
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+		public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+
+			var item = new Item
+			{
+				Text = "Item 1",
+				Description = "This is an item description."
+			};
+
+			viewModel = new ItemDetailViewModel(item);
+			BindingContext = viewModel;
+		}
+
+		void returnClicked(object sender, EventArgs e)
+		{
+			Helper.Return(Description.Text);
+		}
+	}
+}
